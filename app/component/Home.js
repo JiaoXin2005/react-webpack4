ad6d7349162ae9200b08abdb2d40e3cb00b9e2f8
jiaoxin <337471479@qq.com>
2018-06-04 08:21:58 +0800

    2

--- app/component/Home.js
+++ app/component/Home.js
@@ -1,3 +1,64 @@
 import React, { Component } from 'react'
+import { observable, action, toJS} from 'mobx'
+import { observer } from 'mobx-react'
 
-export default () => <div>Home</div>
+var appState = observable({
+  timer: 0
+})
+
+appState.resetTimer = action(function reset(params) {
+  appState.timer = 0
+})
+
+
+var user = observable({
+  name: 'jiaoxin',
+  age: 21
+})
+
+@observer
+class Demo extends Component {
+
+  onReset = () => {
+    this.props.appState.resetTimer()
+  }
+
+  componentWillUnmount() {
+    this.timer && clearInterval(this.timer)
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(action(function tick(params) {
+      appState.timer++
+    }), 1000)
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.appState.timer}
+        <button onClick={this.onReset}>加</button>
+      </div>
+    )
+  }
+}
+
+// @observer
+class Home extends Component {
+  render() {
+    console.log(this)
+    return (
+      <div>
+        <Demo appState={appState}/>
+      </div>
+    )
+  }
+}
+
+
+
+setTimeout(() => {
+  user.name = 'adasad'
+}, 500);
+
+export default Home
--- app/component/User.js
+++ app/component/User.js
@@ -1,3 +1,83 @@
 import React, { Component } from 'react'
+import { observable, computed, action, configure, runInAction, autorun} from 'mobx'
+import { observer } from 'mobx-react'
 
-export default () => <div>User</div>
+
+class TodoState {
+  @observable todos = []
+
+  @computed get completedTodoCount () {
+    return this.todos.filter(todo => todo.completed === true).length
+  }
+
+  @action
+  addTodo(task) {
+    this.todos.push({task, completed: true})
+  }
+
+  @action
+  toggleCompleted(index) {
+    this.todos[index].completed = !this.todos[index].completed
+  }
+
+}
+
+
+@observer
+class TodoList extends Component {
+
+  onAddTodos = () => {
+    setTimeout(() => {
+      this.props.todoState.todos.push({ task: '生化危机', completed: false })
+    }, 500);
+  }
+
+  render() {
+    let {todoState} = this.props
+    return (
+      <div>
+        <button onClick={this.onAddTodos}>add</button>
+        {todoState.todos.map((item, index) => <Todo todo={item} key={index} index={index} todoState={todoState}/>)}
+        Propgress: { todoState.completedTodoCount }
+        
+      </div>
+    )
+  }
+}
+
+@observer
+class Todo extends Component {
+
+  onRename = () => {
+    const todo = this.props.todo
+    todo.task = prompt('输入任务名', todo.task) || ""
+  }
+
+  onToggleCompleted = (index) => {
+    const { todo, todoState} = this.props
+    runInAction(() => {
+      todo.completed = !todo.completed
+    })
+    // todo.completed = !todo.completed
+    // todoState.toggleCompleted(index)
+  }
+
+  render() {
+    let {todo, index } = this.props
+    return (
+      <li onDoubleClick={this.onRename}>
+        <input type="checkbox" checked={todo.completed} onChange={this.onToggleCompleted.bind(this, index)} />
+        {todo.task}
+      </li>
+    )
+  }
+}
+
+configure({ enforceActions: true})
+const todoState = new TodoState()
+todoState.addTodo('塞尔达传说 荒野之息')
+todoState.addTodo('怪物猎人 世界')
+
+
+
+export default () => <TodoList todoState={todoState}/>
